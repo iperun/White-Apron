072e73e292dd8903e2dd6f37931a6a24bbbd662e
Ivan Perun <peruni101@gmail.com>
2018-08-11 10:30:59 -0400

    scrolling added

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,4 +12,34 @@ $(document).ready(function() {
     offset: '60px;'
   });
 
+// button scrolling
+  $('.js--scroll-to-plan').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-plans').offset().top
+    }, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-features').offset().top
+    }, 1000);
+  });
+
+// smooth scrolling
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+
 });
